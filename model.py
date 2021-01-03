cf3f9f7f3c74a0ddc1532551997e1382d1c5843a
ben-wycliff <ben12wycliff@gmail>
2021-01-03 23:42:37 +0300

    Creates VGG block

--- model.py
+++ model.py
@@ -11,5 +11,11 @@ from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.utils import plot_model
 
+def vgg_block(layer_in, n_filters, n_conv):
+    for i in range(n_conv):
+        layer_in = Conv2D(n_filters, (3,3), padding="same", activation="relu")(layer_in)
+    layer_in = MaxPooling2D((2,2), strides=(2,2))(layer_in)
+    return layer_in
+
 
 
