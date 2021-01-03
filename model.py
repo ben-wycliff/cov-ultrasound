b2977625bc21abe11e40ea4857c2206960214a29
ben-wycliff <ben12wycliff@gmail>
2021-01-03 23:33:34 +0300

    Initial commit

---
+++ model.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  3 23:18:21 2021
+
+@author: - Ben Wycliff Mugalu
+"""
+
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.utils import plot_model
+
+
