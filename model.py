7634e7f9fa4b75d89ddc866af3e7d44df08fbbed
ben-wycliff <ben12wycliff@gmail>
2021-01-07 13:58:20 +0300

    Inspects one of the train images

--- model.py
+++ model.py
@@ -11,18 +11,8 @@ from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.utils import plot_model
 from keras.preprocessing.image import ImageDataGenerator
-
-# def vgg_block(layer_in, n_filters, n_conv):
-#     for i in range(n_conv):
-#         layer_in = Conv2D(n_filters, (3,3), padding="same", activation="relu")(layer_in)
-#     layer_in = MaxPooling2D((2,2), strides=(2,2))(layer_in)
-#     return layer_in
-
-# visible = Input(shape=(256, 256, 3))
-# layer = vgg_block(visible, 64, 2)
-# model = Model (inputs=visible, outputs=layer)
-# model.summary()
-# plot_model(model, show_shapes=True, to_file="vgg_block.jpg")
+from PIL import Image
+from matplotlib import pyplot
 
 # Load data progressively
 datagen = ImageDataGenerator()
@@ -35,3 +25,9 @@ testit = datagen.flow_from_directory("data/test", class_mode="categorical")
 #inspecting batch
 batchX, batchy = trainit.next()
 print(f"batch shape = {batchX.shape}, min = {batchX.min()}, max = {batchX.max()}")
+
+image = batchX[0].astype("uint8")
+print(image.shape)
+pyplot.imshow(image)
+pyplot.show()
+
