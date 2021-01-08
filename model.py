1a9732746baf53adf43a9c223040da65ac606a43
ben-wycliff <ben12wycliff@gmail>
2021-01-08 14:44:56 +0300

    Loads pretrained VGG16 model __ Imagenet

--- model.py
+++ model.py
@@ -6,20 +6,19 @@ Created on Sun Jan  3 23:18:21 2021
 """
 
 from keras.models import Model
-from keras.layers import Input
-from keras.layers import Conv2D
-from keras.layers import MaxPooling2D
 from keras.utils import plot_model
 from keras.preprocessing.image import ImageDataGenerator
 from PIL import Image
 from matplotlib import pyplot
 from keras.utils import to_categorical
-
+from keras.applications.vgg16 import VGG16
+from keras.models import Model
+from keras.layers import Dense, Flatten
 # Load data progressively
 datagen = ImageDataGenerator(
     rescale=1.0/255.0,
-    featurewise_center=True,
-    featurewise_std_normalization=True,
+    samplewise_center=True,
+    samplewise_std_normalization=True,
     height_shift_range=0.1, 
     width_shift_range=0.1, 
     horizontal_flip=True, 
@@ -27,9 +26,9 @@ datagen = ImageDataGenerator(
     rotation_range=10)
 
 # create train iteration
-trainit = datagen.flow_from_directory("data/train", target_size=(224, 224), class_mode="categorical")
+trainit = datagen.flow_from_directory("data/train", target_size=(224, 224), class_mode="categorical", batch_size=64)
 # create test iteration
-testit = datagen.flow_from_directory("data/test", target_size=(224, 224), class_mode="categorical")
+testit = datagen.flow_from_directory("data/test", target_size=(224, 224), class_mode="categorical", batch_size=64)
 
 #inspecting batch
 batchX, batchy = trainit.next()
@@ -41,4 +40,5 @@ print(f"batch shape = {batchX.shape}, min = {batchX.min()}, max = {batchX.max()}
 # pyplot.imshow(image)
 # pyplot.show()
 
+model = VGG16(include_top=False, input_shape=(224, 224, 3))
 
