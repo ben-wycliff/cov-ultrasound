81a9760df59a7c2f77c240dfc09b471a6a687ecd
ben-wycliff <ben12wycliff@gmail>
2021-01-08 18:05:07 +0300

    Evaluates model

--- model.py
+++ model.py
@@ -14,6 +14,7 @@ from keras.utils import to_categorical
 from keras.applications.vgg16 import VGG16
 from keras.models import Model
 from keras.layers import Dense, Flatten, BatchNormalization, Dropout
+from keras.optimizers import Adam
 #======================== Load data progressively =============================
 datagen = ImageDataGenerator(
     rescale=1.0/255.0,
@@ -26,9 +27,9 @@ datagen = ImageDataGenerator(
     rotation_range=10)
 
 # create train iteration
-trainit = datagen.flow_from_directory("data/train", target_size=(224, 224), class_mode="categorical", batch_size=64)
+trainit = datagen.flow_from_directory("data/train", target_size=(224, 224), class_mode="categorical", batch_size=32)
 # create test iteration
-testit = datagen.flow_from_directory("data/test", target_size=(224, 224), class_mode="categorical", batch_size=64)
+testit = datagen.flow_from_directory("data/test", target_size=(224, 224), class_mode="categorical", batch_size=32)
 
 #inspecting batch
 batchX, batchy = trainit.next()
@@ -47,13 +48,21 @@ def define_model():
         layer.trainable = False
         
     flat1 = Flatten()(model.layers[-1].output)
-    class1 = Dense(64, activation="relu")(flat1)
+    class1 = Dense(128, activation="relu")(flat1)
     dropout = Dropout(0.5)(class1)
     normalized = BatchNormalization()(dropout)
     output = Dense(3, activation="softmax")(normalized)
     model = Model(inputs=model.inputs, outputs=output)
     model.summary()
     # compile model
-    opt = SGD(lr=0.001, momentum=0.9)
+    opt = Adam(lr=0.0001)
     model.compile(optimizer=opt, loss="categorical_crossentropy", metrics=["accuracy"])
     return model
+
+def train_test_model(train_it, test_it):
+    model = define_model()
+    model.fit(train_it, epochs=128)
+    loss = model.evaluate(test_it, steps=24)
+    print(loss)
+    return model
+train_test_model(trainit, testit)
